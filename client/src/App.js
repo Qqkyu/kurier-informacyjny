20feb76e78bf71b2e2a854703e69a92352c6e6a6
Qqkyu <kulmisiu007@gmail.com>
2021-04-26 23:49:19 +0200

    Installed styled components and created basic navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
+import { Navbar } from "./components";
+
 function App() {
     return (
         <div className="App">
-            <h1>Kurier informacyjny</h1>
+            <Navbar />
         </div>
     );
 }
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Nav } from "./Navbar.elements";
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav></Nav>
+        </>
+    );
+};
+
+export default Navbar;
