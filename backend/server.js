4c85ae79180bf59211c6ba9aca1058fdd2cf605b
Natalia Kramarz <nat.kramarz@gmail.com>
2021-05-09 13:16:27 +0200

    added app routes

--- backend/server.js
+++ backend/server.js
@@ -13,14 +13,10 @@ app.use(cookieParser());
 app.use(cors);
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+app.post("/signup");
+app.post("/signin");
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
-});
+app.get("/api/display_category/:id");
+app.get("/api/user/");
 
 export default app;
