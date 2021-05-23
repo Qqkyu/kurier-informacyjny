6e9edf56b76ad5af75f4f9a025a540a62eeaee77
Natalia Kramarz <nat.kramarz@gmail.com>
2021-05-23 19:17:23 +0200

    added CRUD functions

--- backend/resources/source/source.controllers.js
+++ backend/resources/source/source.controllers.js
@@ -1,4 +1,24 @@
-import { Source } from "./source.model.js";
+import Source from "./source.model.js";
+import { incSourceIdValue } from "./counter/counter.controllers.js";
+
+export const getSources = async (category_id) => {
+    return await Source.find({
+        defaultAssignment: category_id,
+    });
+};
+
+export const getSourcesNames = async (category_id) => {
+    var sources = await getSources(category_id);
+    return sources.map(function (source) {
+        return source.name;
+    });
+};
+
+export const removeSource = async (sourceName) => {
+    return await Source.findOneAndDelete({
+        name: sourceName,
+    });
+};
 
 export const addSource = async (
     sourceName,
@@ -8,7 +28,7 @@ export const addSource = async (
 ) => {
     try {
         const user = await Source.create({
-            _id: 1,
+            _id: await incSourceIdValue(),
             url: sourceUrl,
             description: description,
             name: sourceName,
@@ -16,5 +36,15 @@ export const addSource = async (
         });
     } catch (e) {
         console.log(e);
+    }
+};
+
+export const changeSourceName = async (oldName, newName) => {
+    try {
+        await Source.updateOne({ name: oldName }, { $set: { name: newName } });
+        return true;
+    } catch (e) {
+        console.log(e);
+        return false;
     }
 };
