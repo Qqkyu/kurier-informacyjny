b5bb36fe41801a2f61e36989566aea0165f4b5de
Natalia Kramarz <nat.kramarz@gmail.com>
2021-05-26 13:25:26 +0200

    added dependencies

--- backend/__tests__/category/addSource.unit.test.js
+++
@@ -1,7 +0,0 @@
-describe("addSource", () => {
-    test("should add 'xyz' as a source", async () => {
-        expect.assertions(1);
-        await addSource("xyz", "https://www.example.pl/", "center");
-        await expect(getSources("center")).resolves.toContain("xyz");
-    });
-});
--- backend/__tests__/user/createUser.unit.test.js
+++
@@ -1,7 +0,0 @@
-describe("createUser", () => {
-    test('should create "example@gmail.com" account', async () => {
-        expect.assertions(1);
-        await createUser("example@gmail.com", "passwd");
-        await expect(findUser("example@gmail.com")).resolves.not.toBeNull();
-    });
-});
